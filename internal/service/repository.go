3325a31fd1660f61fdb651f03853e4f24d9122b8
nekitvand <nekitvand@gmail.com>
2022-09-16 21:28:27 +0300

    create GetAllToDo method

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -38,14 +38,29 @@ func (r *Repository) FindAllToDoes(ctx context.Context) (ToDos, error) {
 	return todoes, nil
 }
 
-func (r *Repository) CreateToDo(ctx context.Context, todo *ToDo) (message string,err error) {
-	query := sq.Insert("todo").PlaceholderFormat(sq.Dollar).Columns("id","title","text").Values(todo.Id,todo.Name,todo.Text).RunWith(r.DB)
+func (r *Repository) CreateToDo(ctx context.Context, todo *ToDo) (message string, err error) {
+	query := sq.Insert("todo").PlaceholderFormat(sq.Dollar).Columns("id", "title", "text").Values(todo.Id, todo.Name, todo.Text).RunWith(r.DB)
 
-	_,err = query.ExecContext(ctx)
-	if err != nil{
+	_, err = query.ExecContext(ctx)
+	if err != nil {
 		fmt.Println(err)
 		return "dont added", err
 	}
-	return fmt.Sprint("added todo with id", todo.Id),nil
-	
+	return fmt.Sprint("added todo with id", todo.Id), nil
+
+}
+
+func (r *Repository) GetAllToDo(ctx context.Context) (todoes []*ToDo, err error) {
+	query := sq.Select("*").From("todo")
+
+	s, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+	var todos []*ToDo
+	err = r.DB.SelectContext(ctx, &todos, s, args...)
+	if err != nil {
+		return nil, err
+	}
+	return todos, err
 }
